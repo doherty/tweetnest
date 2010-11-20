4882d20c95157f4ad3963967f9569425c2e7abe8
Thomas Meyer <thomas@m3y3r.de>
2010-11-20 16:35:34 +0100

    There is no need to left join. All users that did tweet something do need an belonging entry in the tweetusers table. this will also utilies the index on the attribute time on table tweets (because of LIMIT 25)

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	// Front page
 	
 	require "inc/preheader.php";
-	$q = $db->query("SELECT `".DTP."tweets`.*, `".DTP."tweetusers`.`screenname`, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."tweets` LEFT JOIN `".DTP."tweetusers` ON `".DTP."tweets`.`userid` = `".DTP."tweetusers`.`userid` ORDER BY `".DTP."tweets`.`time` DESC LIMIT 25");
+	$q = $db->query("SELECT `".DTP."tweets`.*, `".DTP."tweetusers`.`screenname`, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."tweets` JOIN `".DTP."tweetusers` ON `".DTP."tweets`.`userid` = `".DTP."tweetusers`.`userid` ORDER BY `".DTP."tweets`.`time` DESC LIMIT 25");
 	$pageHeader = "Recent tweets";
 	$home       = true;
 	require "inc/header.php";
