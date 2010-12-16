db31b088807c715ac8aa134c55a8c284c3ef8773
Thomas Meyer <thomas@m3y3r.de>
2010-12-16 20:12:03 +0100

    use an INNER JOIN and use 'BETWEEN x and y' for attribute time to utilise the index "tweets_x1"

--- day.php
+++ day.php
@@ -7,8 +7,11 @@
 	$m = ltrim($_GET['m'], "0");
 	$d = ltrim($_GET['d'], "0");
 	if(!is_numeric($d) || !is_numeric($m) || !is_numeric($_GET['y']) || (is_numeric($d) && ($d > 31 || $d < 1)) || (is_numeric($m) && ($m > 12 || $m < 1)) || (is_numeric($_GET['y']) && $_GET['y'] < 2000)){ errorPage("Invalid year, month or day. Please specify a valid date."); }
-	
-	$q = $db->query("SELECT `".DTP."tweets`.*, `".DTP."tweetusers`.`screenname`, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."tweets` LEFT JOIN `".DTP."tweetusers` ON `".DTP."tweets`.`userid` = `".DTP."tweetusers`.`userid` WHERE YEAR(FROM_UNIXTIME(`time`" . DB_OFFSET . ")) = '" . s($_GET['y']) . "' AND MONTH(FROM_UNIXTIME(`time`" . DB_OFFSET . ")) = '" . s($m) . "' AND DAY(FROM_UNIXTIME(`time`" . DB_OFFSET . ")) = '" . s($d) . "' ORDER BY `".DTP."tweets`.`time` DESC");
+
+	$timefrom = strtotime($d.".".$m.".".$_GET['y']) + $dbOffset;
+	$timeto   = strtotime('-1 second', strtotime('+1 day', $timefrom)) + $dbOffset;
+
+	$q = $db->query("SELECT `".DTP."tweets`.*, `".DTP."tweetusers`.`screenname`, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."tweets` JOIN `".DTP."tweetusers` ON `".DTP."tweets`.`userid` = `".DTP."tweetusers`.`userid` WHERE `time` BETWEEN " . $timefrom . " AND " . $timeto . " ORDER BY `".DTP."tweets`.`time` DESC");
 	
 	$selectedDate = array("y" => $_GET['y'], "m" => $m, "d" => $d);
 	$pageTitle    = date("F jS, Y", mktime(1,0,0,$m,$d,$_GET['y']));
--- month.php
+++ month.php
@@ -6,8 +6,11 @@
 	
 	$m = ltrim($_GET['m'], "0");
 	if(!is_numeric($m) || !is_numeric($_GET['y']) || (is_numeric($m) && ($m > 12 || $m < 1)) || (is_numeric($_GET['y']) && $_GET['y'] < 2000)){ errorPage("Invalid year or month"); }
-	
-	$q = $db->query("SELECT `".DTP."tweets`.*, `".DTP."tweetusers`.`screenname`, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."tweets` LEFT JOIN `".DTP."tweetusers` ON `".DTP."tweets`.`userid` = `".DTP."tweetusers`.`userid` WHERE YEAR(FROM_UNIXTIME(`time`" . DB_OFFSET . ")) = '" . $db->s($_GET['y']) . "' AND MONTH(FROM_UNIXTIME(`time`" . DB_OFFSET . ")) = '" . $db->s($m) . "' ORDER BY `".DTP."tweets`.`time` DESC");
+
+	$timefrom = strtotime("01.".$m.".".$_GET['y']) + $dbOffset;
+	$timeto   = strtotime('-1 second', strtotime('+1 month', $timefrom)) + $dbOffset;
+
+	$q = $db->query("SELECT `".DTP."tweets`.*, `".DTP."tweetusers`.`screenname`, `".DTP."tweetusers`.`realname`, `".DTP."tweetusers`.`profileimage` FROM `".DTP."tweets` JOIN `".DTP."tweetusers` ON `".DTP."tweets`.`userid` = `".DTP."tweetusers`.`userid` WHERE `time` BETWEEN " . $timefrom . " AND " . $timeto . " ORDER BY `".DTP."tweets`.`time` DESC");
 	
 	$selectedDate = array("y" => $_GET['y'], "m" => $m, "d" => 0);
 	$pageTitle    = date("F Y", mktime(1,0,0,$m,1,$_GET['y']));
