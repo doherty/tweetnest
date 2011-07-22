76dd01f18bb34710d9c3d5a3db1bb127f86a54ff
Maxime <maxime.m62@gmail.com>
2011-07-22 07:19:15 -0700

    Adios awesome if cascade!
    Welcome switch for simply merge (we merge only line by line for the services host)
    
    Conflicts:
    	extensions/images.php

--- extensions/images.php
+++ extensions/images.php
@@ -1,147 +1,124 @@
 <?php
-	// PONGSOCKET TWEET ARCHIVE
-	// Image display extension
+// PONGSOCKET TWEET ARCHIVE
+// Image display extension
 
-	if(!function_exists("imgid")){
-		function imgid($path){
-			$m = array();
-			preg_match("@/([a-z0-9]+).*@i", $path, $m);
-			if(count($m) > 0){
-				return $m[1];
-			}
-			return false;
-		}
-	}
+if (!function_exists("imgid"))
+{
 
-	class Extension_Images {
-		public function enhanceTweet($tweet){
-			$imgs  = array();
-			$links = findURLs($tweet['text']);
+    function imgid($path)
+    {
+        $m = array();
+        preg_match("@/([a-z0-9]+).*@i", $path, $m);
+        if (count($m) > 0)
+        {
+            return $m[1];
+        }
+        return false;
+    }
+}
+class Extension_Images
+{
 
-			foreach($links as $link => $l){
-				$curl = curl_init($link);
-				curl_setopt_array($curl, array(
-								CURLOPT_USERAGENT => "Mozilla/5.0 (Compatible; libCURL)",
-								CURLOPT_TIMEOUT => 5,
-								CURLOPT_RETURNTRANSFER => 1,
-								CURLOPT_FOLLOWLOCATION => 1,
-								CURLOPT_MAXREDIRS => 3,
-								CURLOPT_NOBODY => 1,
-						));
+    public function enhanceTweet($tweet)
+    {
+        $imgs = array();
+        $links = findURLs($tweet['text']);
+        foreach ($links as $link => $l)
+        {
+            if (is_array($l) && array_key_exists("host", $l) && array_key_exists("path", $l))
+            {
+                $domain = domain($l['host']);
+                $imgid = imgid($l['path']);
+                if ($imgid)
+                {
+                    $http = 'http';
+                    switch ($domain)
+                    {
+                        case 'flic.kr';
+                            $imgid = explode("/",$l['path']);
+                            $c = count($imgid) - 1;
+                            $imgid = $imgid[$c];
+                            $imgs[$link] = $http . "://flic.kr/p/img/".$imgid."_m.jpg";
+                            break;
+                        case 'twimg.com';
+                            $displaylink = $linkmap ? $linkmap[$link] : $link;
+                            $imgs[$displaylink] = $http . "://p.twimg.com" . $l['path'] . ":thumb";
+                            break;
+                        case 'twitpic.com':
+                            $imgs[$link] = $http . "://twitpic.com/show/thumb/" . $imgid;
+                            break;
+                        case 'pict.mobi':
+                            $imgs[$link] = $http . "://pict.mobi/show/thumb/" . $imgid;
+                            break;
+                        case 'yfrog.com':
+                        case 'yfrog.us':
+                            $imgs[$link] = $http . "://yfrog.com/" . $imgid . ".th.jpg";
+                            break;
+                        case 'tweetphoto.com':
+                        case 'pic.gd':
+                        case 'plixi.com':
+                            $imgs[$link] = $http . "://tweetphotoapi.com/api/TPAPI.svc/imagefromurl?size=thumbnail&url=" . $link;
+                            break;
+                        case 'twitgoo.com':
+                            $values = simplexml_load_string(getURL("http://twitgoo.com/api/message/info/" . $imgid));
+                            $imgs[$link] = (string) $values->thumburl;
+                            break;
+                        case 'img.ly':
+                            $imgs[$link] = $http . "://img.ly/show/thumb/" . $imgid;
+                            break;
+                        case 'imgur.com':
+                            $imgs[$link] = $http . "://i.imgur.com/" . $imgid . "s.jpg";
+                            break;
+                        case 'twitvid.com':
+                            $imgs[$link] = $http . "://images.twitvid.com/" . $imgid . ".jpg";
+                            break;
+                        case 'instagr.am':
+                            $html = (string) getURL($link);
+                            preg_match('/<meta property="og:image" content="([^"]+)"\s*\ />/i', $html, $matches);
+                            if(isset($matches[1])){
+                                $imgs[$link] = substr($matches[0], 35, -4);
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        if (count($imgs) > 0)
+        {
+            $tweet['extra']['imgs'] = $imgs;
+        }
+        return $tweet;
+    }
 
-				$longurl = curl_exec($curl);
-				$url = curl_getinfo($curl, CURLINFO_EFFECTIVE_URL);
-				$url = parse_url($url);
-
-				$links[$link] = $url;
-			}
-			$rt = (array_key_exists("rt", $tweetextra) && !empty($tweetextra['rt']));
-			$entities = $rt ? $tweetextra['rt']['extra']['entities'] : $tweetextra['entities'];
-
-			// Let's go
-			$imgs    = array();
-			$text    = $rt ? $tweetextra['rt']['text'] : $tweet['text'];
-			$mtext   = TwitterApi::mediaLinkTweetText($text, $entities);
-			//$text    = TwitterApi::fullLinkTweetText($text, $entities);
-			$links   = findURLs($mtext); // Two link lists because media links might be different from public URLs
-			$flinks  = findURLs($text);
-
-			if(!empty($links) && !empty($flinks)){ // connection between the two
-				$linkmap = array_combine(array_keys($links), array_keys($flinks));
-			}
-
-			$http    = 'http'; // possible to change to https (if all hosts support it)
-
-			foreach($links as $link => $l){
-				if(is_array($l) && array_key_exists("host", $l) && array_key_exists("path", $l)){
-					$domain = domain($l['host']);
-					$imgid  = imgid($l['path']);
-					if($imgid){
-						if($domain == "flic.kr"){
-							$imgid = explode("/",$l['path']);
-							$c = count($imgid) - 1;
-							$imgid = $imgid[$c];
-							$imgs[$link] = $http . "://flic.kr/p/img/".$imgid."_m.jpg";
-						}
-						if($domain == "twimg.com"){
-							$displaylink = $linkmap ? $linkmap[$link] : $link;
-							$imgs[$displaylink] = $http . "://p.twimg.com" . $l['path'] . ":thumb";
-						}
-						if($domain == "twitpic.com"){
-							$imgs[$link] = $http . "://twitpic.com/show/thumb/" . $imgid;
-						}
-						if($domain == "pict.mobi"){
-							$imgs[$link] = $http . "://pict.mobi/show/thumb/" . $imgid;
-						}
-						if($domain == "yfrog.com" || $domain == "yfrog.us"){
-							$imgs[$link] = $http . "://yfrog.com/" . $imgid . ".th.jpg";
-						}
-						if($domain == "tweetphoto.com" || $domain == "pic.gd" || $domain == "plixi.com"){
-							$imgs[$link] = $http . "://tweetphotoapi.com/api/TPAPI.svc/imagefromurl?size=thumbnail&url=" . $link;
-						}
-						if($domain == "twitgoo.com"){
-							$values = simplexml_load_string(getURL($http . "://twitgoo.com/api/message/info/" . $imgid));
-							$imgs[$link] = (string)$values->thumburl;
-						}
-						if($domain == "img.ly"){
-							$imgs[$link] = $http . "://img.ly/show/thumb/" . $imgid;
-						}
-						if($domain == "pict.mobi"){
-							$imgs[$link] = $http . "://pict.mobi/show/thumb/" . $imgid;
-						}
-						if($domain == "imgur.com"){
-							$imgs[$link] = $http . "://i.imgur.com/" . $imgid . "s.jpg";
-						}
-						if($domain == "twitvid.com"){
-							$imgs[$link] = $http . "://images.twitvid.com/" . $imgid . ".jpg";
-						}
-						if($domain == "instagr.am"){
-							$html = (string)getURL($link);
-							preg_match('/<meta property="og:image" content="([^"]+)"\s*\ />/i', $html, $matches);
-							if(isset($matches[1])){
-								$imgs[$link] = $matches[1];
-							}
-						}
-						if($domain == "instagr.am"){
-							$html = (string) getURL($link);
-							preg_match('/<meta property="og:image" content="[^"]+"\/>/i', $html, $matches);
-							if (isset($matches[0]))
-							{
-								$imgs[$link] = substr($matches[0], 35, -4);
-							}
-						}
-					}
-				}
-			}
-			if(count($imgs) > 0){
-				$tweet['extra']['imgs'] = $imgs;
-			}
-			return $tweet;
-		}
-
-		public function displayTweet($d, $tweet){
-			@$tweetextra = unserialize($tweet['extra']);
-			if(array_key_exists("imgs", $tweetextra)){
-				preg_match("/^([\t]+)</", $d, $m); $x = $m[1];
-				$ds    = explode("\n", $d, 2);
-				$imgd  = ""; $i = 1; $is = array();
-				foreach($tweetextra['imgs'] as $link => $img){
-					$imgd .=
-						$x . "\t<a class=\"pic pic-" . s($i) . "\" href=\"" . s($link) . "\">" .
-						"<img src=\"" . s($img) . "\" alt=\"\" /></a>\n";
-					$is[$link] = $i++;
-				}
-				foreach($is as $link => $i){
-					$ds[1] = preg_replace(
-						"/class=\"([^\"]*)\" href=\"" . preg_quote(s($link), "/") . "\"/",
-						"class=\"$1 picl picl-" . s($i) . "\" href=\"" . s($link) . "\"",
-						$ds[1]
-					);
-				}
-				$d     = implode("\n", array($ds[0], rtrim($imgd, "\n"), $ds[1]));
-			}
-			return array($d, $tweet);
-		}
-	}
-
-	$o = new Extension_Images();
+    public function displayTweet($d, $tweet)
+    {
+        @$tweetextra = unserialize($tweet['extra']);
+        if (array_key_exists("imgs", $tweetextra))
+        {
+            preg_match("/^([\t]+)</", $d, $m);
+            $x = $m[1];
+            $ds = explode("\n", $d, 2);
+            $imgd = "";
+            $i = 1;
+            $is = array();
+            foreach ($tweetextra['imgs'] as $link => $img)
+            {
+                $imgd .=
+                        $x . "\t<a class=\"pic pic-" . s($i) . "\" href=\"" . s($link) . "\">" .
+                        "<img src=\"" . s($img) . "\" alt=\"\" /></a>\n";
+                $is[$link] = $i++;
+            }
+            foreach ($is as $link => $i)
+            {
+                $ds[1] = preg_replace(
+                                "/class=\"([^\"]*)\" href=\"" . preg_quote(s($link), "/") . "\"/",
+                                "class=\"$1 picl picl-" . s($i) . "\" href=\"" . s($link) . "\"",
+                                $ds[1]
+                );
+            }
+            $d = implode("\n", array($ds[0], rtrim($imgd, "\n"), $ds[1]));
+        }
+        return $d;
+    }
+}
+$o = new Extension_Images();
