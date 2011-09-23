b8bd55ad3fdf81859534614843aca1ca16ea83fd
Chris Neale <cdn@domain.tld>
2011-09-23 21:56:28 +0100

    Tweet pre-processing to substitute the pre-t.co link originally referenced

--- extensions/entities.php
+++ extensions/entities.php
@@ -6,10 +6,45 @@
 
 	class Extension_Entities {
 		public function enhanceTweet($tweet){
+		// url -> expanded_url
+		    if (!isset($tweet['extra']['entities'])) {
+		      return $tweet;
+		    }
+
+		    $keys = array();
+
+		    foreach($tweet['extra']['entities']->urls as $u) {
+
+			if(!isset($u->display_url) && empty($u->expanded_url)) continue;
+
+			$url = empty($u->expanded_url) ? $u->url : $u->expanded_url;
+
+		        $keys[$u->indices['0']] = substr(
+		          $tweet['text'],
+		          $u->indices['0'],
+		          $u->indices['1'] - $u->indices['0']
+		        );
+		        $replacements[$u->indices['0']] = $url;
+
+		    }
+
+		    ksort($replacements);
+		    $replacements = array_reverse($replacements, true);
+		    $entified_tweet = $tweet['text'];
+		    foreach ($replacements as $k => $v) {
+		      $entified_tweet = substr_replace($entified_tweet, $v, $k, strlen($keys[$k]));
+		    }
+		    $replacements = array(
+		      'replacements' => $replacements,
+		      'keys' => $keys
+		    );
+		    $tweet['text'] = $entified_tweet;
+
 			return $tweet;
 		}
 		
 		public function displayTweet($d, $tweet){
+		// (probably) unnecessary if enhanceTweet() has run already
 			@$tweetextra = unserialize($tweet['extra']);
 			if(array_key_exists("entities", $tweetextra)){
 
